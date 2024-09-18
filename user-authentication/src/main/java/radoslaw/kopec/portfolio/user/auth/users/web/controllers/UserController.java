a5eba8da607fb0154e7973f828eeded370071694
Radoslaw Kopec <radoslawkopec93@gmail.com>
2024-09-18 19:43:11 +0200

    Useless services removed

--- user-authentication/src/main/java/radoslaw/kopec/portfolio/user/auth/users/web/controllers/UserController.java
+++
@@ -1,9 +0,0 @@
-package radoslaw.kopec.portfolio.user.auth.users.web.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-
-
-}
--- user-authentication/src/main/java/radoslaw/kopec/portfolio/user/auth/users/web/dtos/UserLoginRequest.java
+++
@@ -1,4 +0,0 @@
-package radoslaw.kopec.portfolio.user.auth.users.web.dtos;
-
-public record UserLoginRequest(String email, String password) {
-}
